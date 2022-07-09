8a14ed2178e46256308cd7ed377aeb590335310a
Liz Tremblay <liz.tremblay@ibm.com>
2022-07-08 22:54:13 -0500

    Building the site from scrath, working through Nav component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './sass/app.scss';
+import Navigation from './sass/components/Navigation/navigation.js';
+
+const App = () => {
+    return (
+        <Navigation></Navigation>
+    );
+} 
+export default App;
---
+++ src/sass/components/Navigation/navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+
+    return (
+        <div className='header'>
+            <div className='homeIcon'>
+                <img src='./amb-logo.png' alt='CEAS Ambassadors' style={{ width: '20%' }}/>
+            </div>
+        </div>
+    );
+}
+
+export default Navigation;
